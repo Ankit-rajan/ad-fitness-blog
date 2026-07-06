f80bb064809231a7ea6d88ff135f4243051ea3c3
Ankit-rajan <ankitankit0810@gmail.com>
2026-07-07 02:20:27 +0530

    updated project

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { 
+    registerUser,
+     loginUser, 
+
+} = require("../controllers/authController");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Router is working");
+});
+
+router.post("/register", registerUser);
+
+router.post("/login",loginUser);
+
+module.exports = router;
